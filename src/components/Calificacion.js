d8ba5e11eb12d63d16eec4801888ffb0027e2314
Jovanni villalobos <jovanni.villa35@gmail.com>
2025-05-20 16:02:39 -0600

    Update Calificacion.js
    se cambio el estilo de la calificacion

--- src/components/Calificacion.js
+++ src/components/Calificacion.js
@@ -7,10 +7,15 @@ export default function Calificacion ({titulo, calificacion, evaluaciones, subti
             <div className="w-full p-6">
                 <div className="flex flex-col gap-y-6 ">
                     
-                    <div className="flex items-center gap-x-2 flex-row border border-gray-300 rounded-2xl p-8 px-10 justify-around md:max-w-[80%] ">
+                    <div className="flex items-center gap-x-2 flex-row border border-gray-300 rounded-2xl p-8 px-10 justify-around  ">
                         <div className="flex flex-col items-center">
                             <h4 className="text-lg font-nunito">{`${calificacion}`}</h4>
                             <span className="text-3xl font-nunito">{`${estrellas}`}</span>
+                        </div>
+                        <div className="hidden md:block w-px h-16 bg-gray-300"></div>
+                        <div className="hidden md:flex flex-col items-center">
+                            <h4 className="text-lg font-nunito">{`${titulo}`}</h4>
+                            <span className="text-3xl font-nunito">{`${subtitle}`}</span>
                         </div>
                         <div className="w-px h-16 bg-gray-300"></div>
                         <div className="flex flex-col items-center">
@@ -18,8 +23,7 @@ export default function Calificacion ({titulo, calificacion, evaluaciones, subti
                             <span className="text-2xl font-nunito font-bold">Evaluaciones</span>
                         </div>
                     </div>
-                    
-                    <h4 className="text-lg font-nunito">{subtitle}</h4>                    
+                                       
 
                 </div>
             </div>
