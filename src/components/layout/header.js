a7e98ebebd3b09ee134078eb95cd0dbf2448ec30
jovanni villa <Jovanni.villa35@gmail.com>
2025-05-21 15:53:33 -0600

    Update header.js
    cambios en el header cambios solicitados por el cliente

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -7,10 +7,10 @@ export default function Header() {
   return (
 
     <header
-      className={`fixed w-full sticky bg-white shadow-sm top-0 backdrop-blur-lg w-screen  h-[4rem] flex justify-center z-[10] hover:top-0 `}
+      className={`w-full bg-white border-b border-solid border-[#eee] top-0 backdrop-blur-lg h-[8rem] flex justify-center z-[10] hover:top-0 `}
     >
       <div className="flex items-center z-[1] py-4">
-        <div className="relative flex items-center h-[3rem] w-[12rem]">
+        <div className="relative flex items-center h-[4rem] w-[12rem]">
           <Link href="/" passhref>
             <a><Image src={logo} alt={info.companyName} layout="fill" objectFit="contain"/></a>
           </Link>
