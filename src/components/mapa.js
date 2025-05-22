eafaeb076f3f64d0e987954159d118fb8bade0c9
jovanni villa <Jovanni.villa35@gmail.com>
2025-05-22 17:00:07 -0600

    Update mapa.js
    se agrego la ubicacion correcta de Guadalajara Sur

--- src/components/mapa.js
+++ src/components/mapa.js
@@ -6,7 +6,7 @@ const mapa = ({ ciudad, estado, pais, comentario }) => {
       <div className="h-[500px] md:w-[100%] overflow-hidden rounded-3xl">
         {/* Reemplazamos la imagen por el iframe de Google Maps */}
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2219.9293899955774!2d-103.39849345569692!3d20.65079266074899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428adc251df6f57%3A0x649f259025a83c83!2s3cuartos%20Ag!5e0!3m2!1ses!2smx!4v1747874859743!5m2!1ses!2smx"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2964.4943520948136!2d-103.4488361015852!3d20.586878901850287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842f54b8dc35bb6f%3A0xc5acad50c63f0fee!2sRamada%20Encore%20Guadalajara%20Sur!5e0!3m2!1ses!2smx!4v1747953915451!5m2!1ses!2smx"
           width="100%"
           height="100%"
           style={{ border: 0 }}
@@ -15,8 +15,8 @@ const mapa = ({ ciudad, estado, pais, comentario }) => {
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
-      <div className="text-3xl font-semibold">{`${ciudad} ${estado}, ${pais} `}</div>
-      <div className="text-2xl text-justify">{comentario}</div>
+      <div className="text-3xl font-semibold pt-8">{`${ciudad}, ${estado}, ${pais} `}</div>
+      <div className="text-2xl text-justify pt-4">{comentario}</div>
     </div>
   );
 };
