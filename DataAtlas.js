7838e349e036b2a37a54a63e4d75a8db4aad7cc0
jovanni villa <Jovanni.villa35@gmail.com>
2025-05-22 18:07:46 -0600

    Create DataAtlas.js
    se crea el documento para agregar el contenido directamente TODO

---
+++ DataAtlas.js
@@ -0,0 +1,15 @@
+import Calificacion from "./src/components/Calificacion";
+
+export const DataAtlas = {
+    calle: "Calle 1",
+    colonia: "Colonia 1",
+    cp: "12345",
+    ciudad: "Ciudad 1",
+    estado: "Estado 1",
+    pais: "País 1",
+    Calificacion: 4.94,
+    estrellas: "★★★★★",
+    evaluaciones: 197,
+    UrlMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2964.4943520948136!2d-103.4488361015852!3d20.586878901850287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842f54b8dc35bb6f%3A0xc5acad50c63f0fee!2sRamada%20Encore%20Guadalajara%20Sur!5e0!3m2!1ses!2smx!4v1747953915451!5m2!1ses!2smx",
+    longDescrption: "Descripción larga del lugar, incluyendo detalles sobre la ubicación, servicios y características especiales.",
+};
