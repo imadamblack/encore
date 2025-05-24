7ae5ba6f63afdedbf57ef9070b210e0690be622e
jovanni villa <Jovanni.villa35@gmail.com>
2025-05-23 18:36:30 -0600

    Update Calificacion.js
    se realizo el componente modular con la informacion de DataAtlas

--- src/components/Calificacion.js
+++ src/components/Calificacion.js
@@ -7,15 +7,15 @@ export default function Calificacion ({titulo, calificacion, evaluaciones, subti
             <div className="w-full p-6">
                 <div className="flex flex-col gap-y-6 ">
                     
-                    <div className="flex items-center gap-x-2 flex-row border border-gray-300 rounded-2xl p-8 px-10 justify-around  ">
+                    <div className="flex items-center gap-x-4 flex-row border border-gray-300 rounded-2xl p-8 px-10 justify-around  ">
                         <div className="flex flex-col items-center">
                             <h4 className="text-lg font-nunito">{`${calificacion}`}</h4>
                             <span className="text-3xl font-nunito">{`${estrellas}`}</span>
                         </div>
-                        <div className="hidden md:block w-px h-16 bg-gray-300"></div>
+                        <div className="hidden md:block w-px h-16 bg-gray-300 px-0"></div>
                         <div className="hidden md:flex flex-col items-center">
-                            <h4 className="text-lg font-nunito">{`${titulo}`}</h4>
-                            <span className="text-3xl font-nunito">{`${subtitle}`}</span>
+                            <h4 className="text-lg font-nunito text-center">{`${titulo}`}</h4>
+                            <span className="text-3xl font-nunito text-center">{`${subtitle}`}</span>
                         </div>
                         <div className="w-px h-16 bg-gray-300"></div>
                         <div className="flex flex-col items-center">
