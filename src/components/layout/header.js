04163a8fb4ce7c31243a95ee346a407dc76b7c85
Jovanni villalobos <jovanni.villa35@gmail.com>
2025-05-29 12:54:47 -0600

    Update header.js
    se cambio el header conforme al Layout de Fer

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -9,11 +9,19 @@ export default function Header() {
     <header
       className={`w-full bg-white border-b border-solid border-[#eee] top-0 backdrop-blur-lg h-[8rem] flex justify-center z-[20] hover:top-0 md:fixed`}
     >
-      <div className="flex items-center z-[1] py-4">
-        <div className="relative flex items-center h-[4rem] w-[12rem]">
-          <Link href="#" passhref>
-            <a><Image src={logo} alt={info.companyName} layout="fill" objectFit="contain"/></a>
-          </Link>
+      <div className='w-[1120px] flex justify-between px-11 items-center'>
+        <div className="flex items-center z-[1] py-4">
+          <div className="relative flex items-center h-[4rem] w-[12rem]">
+            <Link href="#" passhref>
+              <a><Image src={logo} alt={info.companyName} layout="fill" objectFit="contain"/></a>
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="flex">
+            <div className='text-lg'><button className="bg-gray-300 hover:bg-red-600">¿Vas a otra ciudad</button>
+            </div>
+          </div>
         </div>
       </div>
     </header>
