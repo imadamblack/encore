54403ec9136345a787e3d31fb7497f7ee4307e6d
Jovanni villalobos <jovanni.villa35@gmail.com>
2025-05-29 15:59:03 -0600

    Update Calificacion.js
    modificacion conforme a los cambios solicitados

--- src/components/Calificacion.js
+++ src/components/Calificacion.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
+import i01 from '../../public/Iconos/Arte.svg'
+import i02 from '../../public/Iconos/Arte.svg'
+import i03 from '../../public/Iconos/Arte.svg'
 
-export default function Calificacion ({titulo, calificacion, evaluaciones, subtitle, estrellas}) {
-  return (
+export default function Calificacion ({t_01, t_02, t_03, alto = 64, ancho = 64}) {
+    return (
     <>
         <div className='max-w-[1120px] mx-auto mt-10 px-[30px] w-full relative' >
             <div className="w-full p-6">
@@ -9,18 +13,33 @@ export default function Calificacion ({titulo, calificacion, evaluaciones, subti
                     
                     <div className="flex items-center gap-x-4 flex-row border border-gray-300 rounded-2xl p-8 px-10 justify-around  ">
                         <div className="flex flex-col items-center">
-                            <h4 className="text-lg font-nunito">{`${calificacion}`}</h4>
-                            <span className="text-3xl font-nunito">{`${estrellas}`}</span>
+                            <Image
+                                src={i01}
+                                height={alto}
+                                width={ancho}
+                                alt="Arte"
+                            />
+                            <div className="text-2xl font-nunito font-semibold">{`${t_01}`}</div>
                         </div>
                         <div className="hidden md:block w-px h-16 bg-gray-300 px-0"></div>
-                        <div className="hidden md:flex flex-col items-center">
-                            <h4 className="text-lg font-nunito text-center">{`${titulo}`}</h4>
-                            <span className="text-3xl font-nunito text-center">{`${subtitle}`}</span>
+                        <div className="flex flex-col items-center">
+                            <Image
+                                src={i01}
+                                height={alto}
+                                width={ancho}
+                                alt="Arte"
+                            />
+                            <div className="text-2xl font-nunito font-semibold">{`${t_02}`}</div>
                         </div>
                         <div className="w-px h-16 bg-gray-300"></div>
                         <div className="flex flex-col items-center">
-                            <h4 className="text-lg font-nunito">{`${evaluaciones}`}</h4>
-                            <span className="text-2xl font-nunito font-bold">Evaluaciones</span>
+                            <Image
+                                src={i01}
+                                height={alto}
+                                width={ancho}
+                                alt="Arte"
+                            />
+                            <div className="text-2xl font-nunito font-semibold">{`${t_03}`}</div>
                         </div>
                     </div>
                                        
