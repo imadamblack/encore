2f99ab6d94bca441c92610631a1832a4e056c776
Jovanni villalobos <jovanni.villa35@gmail.com>
2025-05-29 18:51:57 -0600

    Update footer.js
    se actualiza el Footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,12 +3,26 @@ import { info } from "../../../info";
 export default function Footer() {
   return (
     <footer className="relative mb-0  border-t">
-      <div className="bg-black py-6">
-        <div className="container text-white p-8">
-          <p className="text-center ft-0 m-0">
-            Todos los derechos reservados. {info.companyName} ©{" "}
-            {new Date().getFullYear()}
-          </p>
+      <div className="bg-gray-200 py-6">
+        <div className="container max-w-[1120px] px-[30px] row bg-red-500 py-2 justify-between ">
+          <div className="flex flex-row gap-3 max-w-[50%] items-center">
+            <p className="text-center ft-0 m-0">
+              {info.companyName} ©{" "}
+              {new Date().getFullYear()}
+            </p>
+            <p>Privacidad</p>
+            <p>Terminos</p>
+            <p>Mapa del Sitio</p>
+            <p>Informacion de la campaña</p>
+          </div>
+          <div className="max-w-[50%] flex flex-row gap-3 items-center">
+            <p>Espanol(MX)</p>
+            <p>$ MXN</p>
+            <p>Facebook</p>
+            <p>X</p>
+            <p>instagram</p>
+          </div>
+          
         </div>
       </div>
     </footer>
