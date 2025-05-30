775349e44cc6d89db9ce05bcf66138843688ca51
Jovanni villalobos <jovanni.villa35@gmail.com>
2025-05-29 19:10:49 -0600

    Update DataAtlas.js

--- DataAtlas.js
+++ DataAtlas.js
@@ -21,7 +21,12 @@ export const DataAtlas = {
     UrlMap: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2964.4943520948136!2d-103.4488361015852!3d20.586878901850287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842f54b8dc35bb6f%3A0xc5acad50c63f0fee!2sRamada%20Encore%20Guadalajara%20Sur!5e0!3m2!1ses!2smx!4v1747953915451!5m2!1ses!2smx",
     
     //Descripcion larga de la sede
-    longDescrption: "Es para quienes quieren dormir bien, moverse sin perder tiempo y encontrar diseño, arte y funcionalidad sin pagar extra por cada detalle. Es una cadena de hoteles ejecutivos, sí. Pero con alma local, con actitud relajada, con eficiencia real y con espacios pensados para vivir y no solo para dormir. Cada sede vibra distinto: murales de artistas locales, música que te acompaña y estación de huevos para que desayunes rico. No importa si viajas por trabajo, por un evento, por una feria o por placer. En Ramada Encore te mueves como quieres, duermes como necesitas y conectas con la ciudad de verdad.",
+    longDescrption1: "Es para quienes quieren dormir bien, moverse sin perder tiempo y encontrar diseño, arte y funcionalidad sin pagar extra por cada detalle.",
+
+    longDescrption2: "Es una cadena de hoteles ejecutivos, sí. Pero con alma local, con actitud relajada, con eficiencia real y con espacios pensados para vivir y no solo para dormir. Cada sede vibra distinto: murales de artistas locales, música que te acompaña y estación de huevos para que desayunes rico. No importa si viajas por trabajo, por un evento, por una feria o por placer.",
+
+    longDescrption3:"En Ramada Encore te mueves como quieres, duermes como necesitas y conectas con la ciudad de verdad",
+
     
     // Descripcion del Mapa
     mapaDescripcion:'Este hotel informal se encuentra en un edificio luminoso junto a la carretera 80, a 11 km del centro de convenciones Expo Guadalajara y a 21 km del Aeropuerto Internacional Miguel Hidalgo y Costilla.'
